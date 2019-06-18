cd7d56711839540c217a9a30ed8c0c42de468f23
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-06-18 23:23:41 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   get '/users/:slug' do
     @user = User.find_by_slug(params[:slug])
     erb :'users/show'
+    binding.pry
   end
 
   get '/signup' do
